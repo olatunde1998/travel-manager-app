ce0da76b42ea3d4f5b694276a21b1071570cf88f
geodevcode <geodevinstitute@gmail.com>
2023-08-05 22:41:53 +0100

    update button component

--- src/app/components/button/button.tsx
+++ src/app/components/button/button.tsx
@@ -1,50 +1,51 @@
 interface ButtonProps {
-  btnText?: string;
-  className?: string;
-  handleBtnClick?: () => void;
-  btnIcon?: any;
-}
-
-export const Button = ({
-  btnText,
-  className,
-  handleBtnClick,
-}: ButtonProps) => {
-  return (
-    <>
-      <div
-        className={`${className} p-3 w-[200px] rounded-md text-center cursor-pointer`}
-      >
-        <button
-          onClick={handleBtnClick}
-          className="flex items-center justify-center"
+    btnText?: string;
+    className?: string;
+    handleBtnClick?: () => void;
+    btnIcon?: any;
+  }
+  
+  export const Button = ({
+    btnText,
+    className,
+    handleBtnClick,
+  }: ButtonProps) => {
+    return (
+      <>
+        <div
+          className={`${className} p-3 w-[200px] rounded-md text-center cursor-pointer`}
         >
-          {btnText}
-        </button>
-      </div>
-    </>
-  );
-};
-
-export const IconButton = ({
-  btnText,
-  className,
-  handleBtnClick,
-  btnIcon,
-}: ButtonProps) => {
-  return (
-    <>
-      <div
-        className={`${className} p-3 w-[200px] rounded-md text-center cursor-pointer`}
-      >
-        <button
-          onClick={handleBtnClick}
-          className="flex items-center justify-center"
+          <button
+            onClick={handleBtnClick}
+            className="flex items-center justify-center"
+          >
+            {btnText}
+          </button>
+        </div>
+      </>
+    );
+  };
+  
+  export const IconButton = ({
+    btnText,
+    className,
+    handleBtnClick,
+    btnIcon,
+  }: ButtonProps) => {
+    return (
+      <>
+        <div
+          className={`${className} p-3 w-[200px] rounded-md text-center cursor-pointer`}
         >
-          <span className="mr-4 ml-2">{btnIcon}</span>
-          {btnText}
-        </button>
-      </div>
-    </>
-  );
-};
+          <button
+            onClick={handleBtnClick}
+            className="flex items-center justify-center"
+          >
+            <span className="mr-4 ml-2">{btnIcon}</span>
+            {btnText}
+          </button>
+        </div>
+      </>
+    );
+  };
+  
