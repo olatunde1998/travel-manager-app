0047a0926b46d253c875b4a1a71668a3e350eccf
geodevcode <geodevinstitute@gmail.com>
2023-08-06 18:38:40 +0100

    router added to login button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import { Button, Input } from "./components/index";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,7 +12,9 @@ export default function Home() {
             <div className="flex items-center justify-between">
               <div className="">
                 <p className="font-bold">Login</p>
-                <p className="text-[12px] md:text-[14px]">Login to your account</p>
+                <p className="text-[12px] md:text-[14px]">
+                  Login to your account
+                </p>
               </div>
               <div className="w-[50px] h-[50px] md:w-[100px] md:h-[100px]">
                 <Image
@@ -24,10 +27,12 @@ export default function Home() {
             </div>
             <Input type="text" label="Email" className="mt-8" />
             <Input type="password" label="Password" className="mt-6" />
-            <Button
-              className="bg-[#DDAA33] w-full rounded-none text-white mt-6 flex justify-center mx-auto md:w-[350px] md:mt-8 "
-              btnText="Login"
-            />
+            <Link href="/workspace">
+              <Button
+                className="bg-[#DDAA33] w-full rounded-none text-white mt-6 flex justify-center mx-auto md:w-[350px] md:mt-8 "
+                btnText="Login"
+              />
+            </Link>
           </form>
         </div>
       </main>
